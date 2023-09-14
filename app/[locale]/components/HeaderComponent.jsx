a16edaa9c2ab5d6fd140c310abb50526a85e60cd
GmalFari <gmalfari926@gmail.com>
2023-09-14 17:12:19 +0300

    a

--- app/[locale]/components/HeaderComponent.jsx
+++ app/[locale]/components/HeaderComponent.jsx
@@ -3,6 +3,7 @@ import { Facebook,Instagram,Youtube,Search,AlignJustify } from 'lucide-react'
 import LanguageChanger from './LanguageCanger'
 import { useTranslations } from 'next-intl';
 import { useState } from 'react';
+import Link from 'next/link';
 const HeaderComponent = () => {
     const t = useTranslations('header')
     const [isHidden,setIsHidden]=useState(true)
@@ -55,27 +56,28 @@ const HeaderComponent = () => {
 
         `}>
        
-          <li className="my-4"><a href="/">{t('nav.about')}</a></li>
-          <li className="my-4"><a href="/faculties">{t('nav.academic')}</a></li>
-          <li className="my-4"><a href="/academic_affairs">{t('nav.research')}</a></li>
-          <li className="my-4"><a href="/academic_affairs">{t('nav.student')}</a></li>
+          <li className="my-4"><Link href="/">{t('nav.about')}</Link></li>
+          <li className="my-4"><Link href="/faculties">{t('nav.academic')}</Link></li>
+          <li className="my-4"><Link href="/academic_affairs">{t('nav.research')}</Link></li>
+          <li className="my-4"><Link href="/academic_affairs">{t('nav.student')}</Link></li>
 
-          <li><a href="/research_studies">{t('nav.uni_hospital')}</a></li>
-        
+          <li><Link href="/research_studies">{t('nav.uni_hospital')}</Link></li>
       </nav>
         <div className='md:hidden'>
-        <button className='md:hidden' onClick={handleDropDwon}><AlignJustify/></button>
+        <button className='md:hidden' onClick={handleDropDwon}></button>
       <nav className={`absolute  list-none right-0 mt-8
        py-2 w-48 bg-gray-200 rounded-md  md:hidden shadow-lg z-1000 
             ${isHidden  ?'block':'hidden'}
         `}>
        
-          <li className="my-4 border-2 border-b-gray-300"><a href="/">{t('nav.about')}</a></li>
-          <li className="my-4 border-2 border-b-gray-300"><a href="/faculties">{t('nav.academic')}</a></li>
-          <li className="my-4 border-2 border-b-gray-300"><a href="/academic_affairs">{t('nav.research')}</a></li>
-          <li className="my-4 border-2 border-b-gray-300"><a href="/academic_affairs">{t('nav.student')}</a></li>
+          <li className="my-4 border-2 border-b-gray-300"><Link href="/">{t('nav.about')}</Link></li>
+          <li className="my-4 border-2 border-b-gray-300"><Link href="/faculties">{t('nav.academic')}</Link></li>
+          <li className="my-4 border-2 border-b-gray-300">
+              <Link href="/academic_affairs">{t('nav.research')}</Link></li>
+          <li className="my-4 border-2 border-b-gray-300"><Link href="/academic_affairs">{t('nav.student')}
+          </Link></li>
 
-          <li><a href="/research_studies">{t('nav.uni_hospital')}</a></li>
+          <li><Link href="/research_studies">{t('nav.uni_hospital')}</Link></li>
         
       </nav>
         </div>
